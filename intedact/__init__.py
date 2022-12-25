8a7a1866a0f213666c7a818e3e4f6930187a383a
Matt Boggess <mboggess@nextdoor.com>
2022-12-25 13:10:35 -0800

    v0.1.0 release

--- intedact/__init__.py
+++ intedact/__init__.py
@@ -1,19 +1,18 @@
-from .univariate_eda_interact import univariate_eda_interact
 from .bivariate_eda_interact import bivariate_eda_interact
 from .bivariate_summaries import (
-    numeric_numeric_summary,
-    numeric_categorical_summary,
-    categorical_numeric_summary,
     categorical_categorical_summary,
+    categorical_numeric_summary,
+    numeric_categorical_summary,
+    numeric_numeric_summary,
 )
+from .univariate_eda_interact import univariate_eda_interact
 from .univariate_summaries import (
     categorical_summary,
-    numeric_summary,
-    datetime_summary,
-    text_summary,
     collection_summary,
+    datetime_summary,
+    numeric_summary,
+    text_summary,
     url_summary,
 )
 
-
-__version__ = "0.0.1"
+__version__ = "0.1.0"
